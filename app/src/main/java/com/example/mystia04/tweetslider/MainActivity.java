5c53de5c45d50bca0d8d66673bd6ecd405812af8
mystia04 <12jk220@ms.dendai.ac.jp>
2015-07-02 21:41:43 +0900

    ActionBarがどっかいった(真顔)

--- app/src/main/java/com/example/mystia04/tweetslider/MainActivity.java
+++ app/src/main/java/com/example/mystia04/tweetslider/MainActivity.java
@@ -113,19 +113,6 @@ public class MainActivity extends ListActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
+
